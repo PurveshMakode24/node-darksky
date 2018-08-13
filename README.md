a858984ced5035bf8d11484142c567022dc718b3
Purvesh Makode <makodepurvesh@gmail.com>
2018-08-14 03:40:54 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Node-Darksky
+Help getting an idea of weather for your area/city. Just enter your city name and you are done.
---
+++ server.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var app = express();
+const bodyParser = require('body-parser');
+const request = require('request');
+const apiKey = '***********************';
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine','ejs');
+
+app.get('/', function (req, res) {
+  res.render('index', {weather: null, error: null});
+})
+
+app.post('/', function (req, res) {
+  let city = req.body.city;
+  let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
+
+  request(url, function (err, response, body) {
+    if(err){
+      res.render('index', {weather: null, error: 'Error, please try again'});
+    } else {
+      let weather = JSON.parse(body)
+      if(weather.main == undefined){
+        res.render('index', {weather: null, error: 'Error, please try again'});
+      } else {
+        let weatherText = `It's ${weather.main.temp} degrees fahrenheit in ${weather.name}!`;
+        res.render('index', {weather: weatherText, error: null});
+      }
+    }
+  });
+})
+
+app.listen(3000, function () {
+  console.log('Head over to localhost:3000 in your browser')
+})
